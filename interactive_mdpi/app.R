22fd2d801d6745c36fdec634dc1d7bd53645c49b
Gregory Ho <gregoryh@uchicago.edu>
2024-03-05 23:55:58 -0600

    commit final edits

--- interactive_mdpi/app.R
+++ interactive_mdpi/app.R
@@ -9,27 +9,31 @@
 
 library(shiny)
 library(leaflet)
-library(sf) #This is the culprit that made me spent 1++ hours troubleshooting
+library(sf)
 
 data_path <- readRDS("factor_scores_geoids.rds")
 
 # Define UI for application
 ui <- fluidPage(
-  titlePanel("Interactive Map of Deprivation Factors"),
+  titlePanel("Figure 2: Spatial Distribution of Poverty & Latent Factors"),
   sidebarLayout(
     sidebarPanel(
       selectInput("selected_factor", "Select Factor:", 
                   choices = c(
-                    #Names simplified 
+                    #Names simplified, also for completeness
                               "Poverty Rate" = "poverty_rate", 
-                              "Educational and Health Deprivation" = "Education and Health", 
-                              "Employment and Housing Condition" = "Employment and Shelter", 
-                              "Labor Force Participation and Housing Affordability" = "Labor and Housing Cost", 
+                              "Education and Health" = "Education and Health", 
+                              "Employment and Shelter" = "Employment and Shelter", 
+                              "Labor and Housing Cost" = "Labor and Housing Cost", 
                               "The Digital Divide" = "The Digital Divide", 
-                              "School Attendance" = "School Attendance"))
+                              "School Attendance" = "School Attendance")),
+      checkboxInput("chicago_only", "Show only Chicago", value = FALSE) 
     ),
     mainPanel(
-      leafletOutput("interactiveMap", height = "80vh")
+      leafletOutput("interactiveMap", height = "80vh"),
+      # Added a note at the bottom of the main panel to help make reading cloropleth map more intuitive
+      tags$hr(),
+      tags$p("Note: Higher numbers indicate greater levels of deprivation. Darker colors on the map correspond to areas with higher deprivation scores.")
     )
   )
 )
@@ -37,29 +41,28 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
   output$interactiveMap <- renderLeaflet({
-    # Create the leaflet map and bind data_path as the data source
-    leafletMap <- leaflet(data = data_path) %>%
+    leaflet(data = data_path) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       setView(lng = -87.8, lat = 41.8, zoom = 9)
+  })
+  
+  observe({
+    filtered_data <- if(input$chicago_only) {
+      data_path %>% filter(community_name != "Cook County - Outside Chicago")
+    } else {
+      data_path
+    }
     
-    # Dynamically determine the fillColor based on the selected factor
-    colorPal <- colorBin(palette = "YlOrRd", domain = data_path[[input$selected_factor]], bins = 10)
-    fillColor <- reactive({ colorPal(data_path[[input$selected_factor]]) })
-    
-    # Observe changes in input$selected_factor and update the polygons
-    observe({
-      leafletProxy("interactiveMap", data = data_path) %>%
-        clearShapes() %>%
-        addPolygons(fillColor = ~fillColor(), 
-                    weight = 1,
-                    color = "#BDBDC3",
-                    fillOpacity = 0.8,
-                    popup = ~paste("GeoID: ", GEOID,
-                                   "<br>Score: ", round(get(input$selected_factor), 2),
-                                   "<br>Average Neighbor Score: ", round(get(paste0(input$selected_factor, "_neighbor_average")), 2)))
-    })
-    
-    return(leafletMap)
+    leafletProxy("interactiveMap", data = filtered_data) %>%
+      clearShapes() %>%
+      addPolygons(fillColor = ~colorQuantile(palette = "YlOrRd", domain = filtered_data[[input$selected_factor]], n = 10)(filtered_data[[input$selected_factor]]),
+                  weight = 1,
+                  color = "#BDBDC3",
+                  fillOpacity = 0.7,
+                  popup = ~paste("Community: ", community_name,
+                                 "<br>GeoID: ", GEOID,
+                                 "<br>Score: ", round(filtered_data[[input$selected_factor]][which(filtered_data$GEOID == GEOID)], 2),
+                                 "<br>Average Neighbor Score: ", round(filtered_data[[paste0(input$selected_factor, "_neighbor_average")]][which(filtered_data$GEOID == GEOID)], 2)))
   })
 }
 
